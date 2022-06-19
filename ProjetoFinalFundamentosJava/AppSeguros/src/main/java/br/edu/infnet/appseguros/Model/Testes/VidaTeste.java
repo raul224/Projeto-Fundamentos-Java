8a42e2a091a4b9db3651e9116047bce75026f470
Raul Pires <raulpires224@gmail.com>
2022-06-19 13:54:45 -0300

    Ajuste na classe de teste de vida

--- ProjetoFinalFundamentosJava/AppSeguros/src/main/java/br/edu/infnet/appseguros/Model/Testes/VidaTeste.java
+++ ProjetoFinalFundamentosJava/AppSeguros/src/main/java/br/edu/infnet/appseguros/Model/Testes/VidaTeste.java
@@ -4,10 +4,17 @@ import br.edu.infnet.appseguros.Model.Domain.Vida;
 
 public class VidaTeste {
     public static void main(String[] args) {
-        Vida vida = new Vida("1234", "20/03/2021", 180, 150,
-                20000, true, "18593655742", 1997, "18222720767");
+        try{
+            Vida vida = new Vida("1234", "20/03/2021", 180, 150,
+                    20000, true);
+            vida.setCpf("18593655742");
+            vida.setAnoNascimento(1997);
+            vida.setCpfBeneficiario("18222720767");
+            System.out.println(vida.toString());
+            System.out.println("Valor a receber: " + vida.CalculaValorReceber());
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
 
-        System.out.println(vida.toString());
-        System.out.println("Valor a receber: " + vida.CalculaValorReceber());
     }
 }
